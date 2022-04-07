4a25a80a840aee360d5b218176860134e8019727
DhanielBatista <dhanielbatista@gmail.com>
2022-04-07 16:49:25 -0300

    Initial commit

---
+++ BitbucketLog/Program.cs
@@ -0,0 +1,26 @@
+﻿using BitbucketLog;
+using System.Net;
+using System.Text;
+
+if (string.IsNullOrEmpty(args[0]))
+{
+    Console.WriteLine("You forgot to define the file path as a parameter.");
+    return;
+}
+var fileLines = File.ReadLines(args[0]);
+if (fileLines.Count() <= 0)
+{
+    Console.WriteLine("file don't has content");
+    return;
+}
+
+foreach (var line in fileLines)
+
+{
+    var myReq = (HttpWebRequest)WebRequest.Create("https://api.bitbucket.org/2.0/users/"+line);
+    myReq.Method = "GET";
+    var response = myReq.GetResponse();
+    var stringResponse = Utils.GetStringFromStream(Encoding.UTF8, response.GetResponseStream());
+    File.AppendAllText("log-bitbucket.txt", stringResponse+"\n\n");
+    Thread.Sleep(5000);
+}
---
+++ BitbucketLog/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitbucketLog
+{
+    public static class Utils
+    {
+        public static string GetStringFromStream(Encoding encoding, Stream stream)
+        {
+            var streamReader = new StreamReader(stream, encoding);
+            return streamReader.ReadToEnd();
+        }
+    }
+}
